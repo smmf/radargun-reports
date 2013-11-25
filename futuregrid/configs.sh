ecd8583b5d3c1eebdb3a308d897376b3789e37c5
Sérgio Miguel Fernandes <sergio.fernandes@ist.utl.pt>
2013-11-25 14:02:04 +0000

    FutureGrid: RW{01,05,10,20} for {100,1000,10000} 1p using 0mq (WITH AFFINITY and HWM=0) (EHCACHE and FF-LF) (should go to thesis)

--- futuregrid/configs.sh
+++ futuregrid/configs.sh
@@ -4,5 +4,6 @@ WTP="01 05 10 20"
 NE="100 1000 10000"
 WP="1"
 # P="tdmock-g2800ns-p67000ns-c64000ns fflf-tdmock-g2800ns-p67000ns-c64000ns"
-P="ispn53-repl-sync-rc fflf-ispn53-repl-sync-rc"
+# P="ispn53-repl-sync-rc fflf-ispn53-repl-sync-rc"
+P="ehcache26-repl-sync fflf-ehcache26-repl-sync"
 NODES=`seq 2 2 12`
