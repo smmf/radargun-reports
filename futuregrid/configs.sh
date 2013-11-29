5a0adfe2892cb4541f760d0adbc9d52ca2316e37
Sérgio Miguel Fernandes <sergio.fernandes@ist.utl.pt>
2013-11-29 12:38:10 +0000

    update futuregrid/configs.sh

--- futuregrid/configs.sh
+++ futuregrid/configs.sh
@@ -5,5 +5,6 @@ NE="100 1000 10000"
 WP="1"
 # P="tdmock-g2800ns-p67000ns-c64000ns fflf-tdmock-g2800ns-p67000ns-c64000ns"
 # P="ispn53-repl-sync-rc fflf-ispn53-repl-sync-rc"
-P="ehcache26-repl-sync fflf-ehcache26-repl-sync"
+# P="ehcache26-repl-sync fflf-ehcache26-repl-sync"
+P="fflb-tdmock-g2800ns-p67000ns-c64000ns fflb-ispn53-repl-sync-rc fflb-ehcache26-repl-sync"
 NODES=`seq 2 2 12`
