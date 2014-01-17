b7fbca5346ea09940d3cb243e967f5b9111d35ff
Sérgio Miguel Fernandes <sergio.fernandes@ist.utl.pt>
2014-01-17 15:37:34 +0000

    minor: add some missing plots

--- generate-plot.sh
+++ generate-plot.sh
@@ -27,7 +27,9 @@ cat > "${plotfile}" <<EOF
 reset
 
 #set terminal svg fname 'DejaVu Sans' fsize '9' rounded dashed
-set terminal pdfcairo font 'DejaVu Sans,9' rounded dashed
+# set terminal pdfcairo font 'DejaVu Sans,9' rounded dashed mono enhanced
+set terminal pdfcairo font 'DejaVu Sans,9' rounded dashed enhanced
+#set terminal pdfcairo rounded dashed enhanced
 #set terminal pdf font rounded dashed
 set output 'plot.pdf'
 set datafile separator ","
@@ -45,25 +47,62 @@ set tics out nomirror
 # color definitions
 #set style line 1 lc rgb '#d80073' lw 2.5 ps 2 # --- ISPN
 #set style line 2 lc rgb '#a20025' lw 2.5 ps 2 # --- ISPN + LB
-set style line 1 lt 1 lc -1 pt 4 # --- ISPN
-set style line 2 lt 1 lc  1 pt 5 # --- ISPN + LB
-set style line 3 lt 4 lc  2 pt 5 # --- ISPN + LF
-set style line 4 lt 1 lc -1 pt 8 # --- Hazelcast
-set style line 5 lt 1 lc  1 pt 9 # --- Hazelcast + LB
-set style line 6 lt 4 lc  2 pt 9 # --- Hazelcast + LF
+# set style line 1 lt 1 lc -1 pt 4 # --- ISPN
+# set style line 2 lt 1 lc  1 pt 5 # --- ISPN + LB
+# set style line 3 lt 4 lc  2 pt 5 # --- ISPN + LF
+# set style line 4 lt 4 lc 4 pt 14 # --- Hazelcast
+# set style line 5 lt 1 lc  1 pt 9 # --- Hazelcast + LB
+# set style line 6 lt 4 lc  2 pt 9 # --- Hazelcast + LF
+#
+# set style line 10 lt 1 lc -1 pt 8 # --- hazel
+# set style line 11 lt 4 lc -1 pt 8 # --- hazel + lb
+# set style line 12 lt 3 lc -1 pt 9 # --- hazel + lf
+#
+# set style line 20 lt 1 lc 1 pt 10 # --- ispn53
+# set style line 21 lt 4 lc 1 pt 10 # --- ispn53 + lb
+# set style line 22 lt 3 lc 1 pt 11 # --- ispn53 + lf
+#
+# set style line 30 lt 1 lc 2 pt 12 # --- ehcache
+# set style line 31 lt 4 lc 2 pt 12 # --- ehcache + lb
+# set style line 32 lt 3 lc 2 pt 13 # --- ehcache + lf
+#
+# set style line 40 lt 1 lc 3 pt 6 # --- mock
+# set style line 41 lt 4 lc 3 pt 6 # --- mock + lb
+# set style line 42 lt 3 lc 3 pt 7 # --- mock + lf
+
+set style line 10 lt 1 lc 3 pt 8 # --- hazel
+set style line 11 lt 4 lc 4 pt 9 # --- hazel + lb
+set style line 12 lt 2 lc -1 pt 8 # --- hazel + lf
+
+set style line 20 lt 1 lc 3 pt 10 # --- ispn53
+set style line 21 lt 4 lc 4 pt 11 # --- ispn53 + lb
+set style line 22 lt 2 lc -1 pt 10 # --- ispn53 + lf
+
+set style line 30 lt 1 lc 3 pt 12 # --- ehcache
+set style line 31 lt 4 lc 4 pt 13 # --- ehcache + lb
+set style line 32 lt 2 lc -1 pt 12 # --- ehcache + lf
+
+set style line 40 lt 1 lc 3 pt 6 # --- mock
+set style line 41 lt 4 lc 4 pt 7 # --- mock + lb
+set style line 42 lt 2 lc -1 pt 6 # --- mock + lf
+
+set key left top Right
+#set key at 10,50
+#set key samplen 2.5 spacing 2.0
+
 
 #set xtics (1,4,8,12,16,20,24,28,32,36,40,44,48) font "DejaVu Sans,17" offset 0, graph -0.02
-set xtics font "DejaVu Sans,14" offset 0, graph -0.02
+set xtics font "DejaVu Sans,10" offset 0, graph -0.02
 #set xrange [1:48]
-set xlabel "# Nodes" font "DejaVu Sans,17" offset 0, graph -0.03
+set xlabel "Nodes" font "DejaVu Sans,10" offset 0, graph -0.03
 
-set ytics font "DejaVu Sans,14"
+set ytics font "DejaVu Sans,10"
 #set yrange [:]
-set ylabel "Throughput (1000 txs/sec)" font "DejaVu Sans,17" offset -3
+set ylabel "Throughput (1000 txs/sec)" font "DejaVu Sans,10" offset -3
 
 #unset key
 
-set title "${WORKLOAD}" font "DejaVu Sans,21"
+set title "${WORKLOAD}" font "DejaVu Sans,12"
 
 plot \\
 EOF
